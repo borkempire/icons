73456e4d562583e1a3e1e0176ab09ce1cb2c42aa
Even Nordstad <even.nordstad@scania.com>
2023-08-17 14:20:16 +0200

    test actions

--- update_readme.py
+++ update_readme.py
@@ -6,32 +6,34 @@ def generate_icon_urls(directory):
     for filename in os.listdir(directory):
         if filename.endswith(".png"):  # Adjust file extension as needed
             raw_url = f"https://raw.githubusercontent.com/borkempire/icons/actions/icons/{filename}"
-            icon_urls.append(f"- [{filename}]({raw_url})")
+            icon_urls.append(raw_url)  # Append raw_url to the list
     return icon_urls
 
 
 def update_readme(readme_path, icon_urls):
     with open(readme_path, "r") as f:
-        lines = f.readlines()
+        readme_content = f.read()
 
     # Find the section where you want to insert the URLs, e.g., <!-- ICONS START --> and <!-- ICONS END -->
     start_marker = "<!-- ICONS START -->"
     end_marker = "<!-- ICONS END -->"
-    start_index = None
-    end_index = None
 
-    for i, line in enumerate(lines):
-        if start_marker in line:
-            start_index = i
-        if end_marker in line:
-            end_index = i
+    start_index = readme_content.find(start_marker)
+    end_index = readme_content.find(end_marker)
 
-    if start_index is not None and end_index is not None:
+    if start_index != -1 and end_index != -1:
+        # Generate the Markdown list of raw URLs
+        raw_urls_list = "\n".join([f"- {url}" for url in icon_urls])
+
         # Update the section with the generated URLs
-        updated_lines = lines[: start_index + 1] + icon_urls + lines[end_index:]
+        updated_readme = (
+            readme_content[: start_index + len(start_marker)]
+            + f"\n{raw_urls_list}\n"
+            + readme_content[end_index:]
+        )
 
         with open(readme_path, "w") as f:
-            f.writelines(updated_lines)
+            f.write(updated_readme)
 
 
 if __name__ == "__main__":
